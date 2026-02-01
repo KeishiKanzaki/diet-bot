5cfd78a0ad2a1ca937efb1bbe3fcfae19e3cd854
Kanzaki <k.keishi11.24@gmail.com>
2026-02-01 19:27:51 +0900

    change gemini version

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ configuration = Configuration(access_token=LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-3.0-flash') 
+model = genai.GenerativeModel("gemini-3-flash-preview") 
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
